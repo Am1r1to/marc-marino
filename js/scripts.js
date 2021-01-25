dda290faea3c3d74490c04ef71ea96b3216919d7
Amira <amhajjem@gmail.com>
2021-01-25 23:48:04 +0100

    midi2

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,13 @@ var swiper = new Swiper('.swiper-container', {
       prevEl: '.swiper-button-prev',
     },
   });
+
+  ScrollReveal({ reset: true });
+  // ScrollReveal().reveal('.article');
+  ScrollReveal().reveal('.article',{ distance: '50px',  duration: 1000 });
+  ScrollReveal().reveal('.images',{ distance: '50px',  duration: 1000 });
+  ScrollReveal().reveal('.article2',{ distance: '50px',  duration: 1000 });
+  ScrollReveal().reveal('.swiper-container',{ distance: '50px',  duration: 1000 });
+  ScrollReveal().reveal('.pc',{ distance: '50px',  duration: 1000 });
+  ScrollReveal().reveal('.bg1',{ distance: '50px',  duration: 1000 });
+  ScrollReveal().reveal('.bg2',{ distance: '50px',  duration: 1000 });
