4713bb133bb5edc739bc3f753021a90ebe196b03
Amira <amhajjem@gmail.com>
2021-01-26 10:45:07 +0100

    10h

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,20 @@
-var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
 
-  // ScrollReveal({ reset: true });
-  // // ScrollReveal().reveal('.article');
-  // ScrollReveal().reveal('.article',{ distance: '50px',  duration: 1000 });
-  // ScrollReveal().reveal('.images',{ distance: '50px',  duration: 1000 });
-  // ScrollReveal().reveal('.article2',{ distance: '50px',  duration: 1000 });
-  // ScrollReveal().reveal('.swiper-container',{ distance: '50px',  duration: 1000 });
-  // ScrollReveal().reveal('.pc',{ distance: '50px',  duration: 1000 });
-  // ScrollReveal().reveal('.bg1',{ distance: '50px',  duration: 1000 });
-  // ScrollReveal().reveal('.bg2',{ distance: '50px',  duration: 1000 });
+
+
+  document.addEventListener('DOMContentLoaded', function(){
+    let contact = document.querySelector('.lien-contact');
+    let close = document.querySelector('.close');
+
+    let menu = document.querySelector ('.contact');
+ 
+    contact.addEventListener('click', function(){
+        menu.classList.add('closed');
+       
+    })
+
+    close.addEventListener('click', function(){
+      menu.classList.remove('closed');
+     
+  })
+ })
+
